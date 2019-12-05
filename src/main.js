734adc202350eabf2d2a38cda1d39774b1e8058a
liuchunyu <852933072@qq.com>
2019-12-05 10:31:24 +0800

    11

--- src/main.js
+++ src/main.js
@@ -9,8 +9,69 @@ import 'element-ui/lib/theme-chalk/index.css'; // 默认主题
 import './assets/css/icon.css';
 import './components/common/directives';
 import 'babel-polyfill';
+import axios from 'axios';
+import Vuex from 'vuex';
 
+Vue.prototype.axios = axios;
+Vue.use(Vuex);
 Vue.config.productionTip = false;
+/*new Vue({
+    el: '#app',
+    router,
+    components: { App },
+    template: '<App/>'
+});*/
+
+
+const ADD_COUNT = 'ADD_COUNT'; // 用常量代替事件类型，使得代码更清晰
+const REMOVE_COUNT = 'REMOVE_COUNT';
+//注册状态管理全局参数
+var store = new Vuex.Store({
+    state:{
+        token:'',
+        userID:'',
+    },
+    mutations: {
+        //写法与getters相类似
+        //组件想要对于vuex 中的数据进行的处理
+        //组件中采用this.$store.commit('方法名') 的方式调用，实现充分解耦
+        //内部操作必须在此刻完成(同步)
+        [ADD_COUNT] (state, token) { // 第一个参数为 state 用于变更状态 登录
+            sessionStorage.setItem("token", token);
+            state.token = token;
+        },
+        [REMOVE_COUNT] (state, token) { // 退出登录
+
+            sessionStorage.removeItem("token", token);
+
+            state.token = token;
+        },
+    }
+});
+/*
+router.beforeEach((to, from, next) => {
+    iView.LoadingBar.start();//loadong 效果
+
+    store.state.token = sessionStorage.getItem('token');//获取本地存储的token
+
+    if (to.meta.requireAuth) {  // 判断该路由是否需要登录权限
+        if (store.state.token !== "") {  // 通过vuex state获取当前的token是否存
+            next();
+        }
+        else {
+            next({
+                path: '/login',
+                query: {redirect: to.fullPath}  // 将跳转的路由path作为参数，登录成功后跳转到该路由
+            })
+        }
+    }
+    else {
+        next();
+    }
+})*/
+
+
+
 Vue.use(VueI18n);
 Vue.use(ElementUI, {
     size: 'small'
@@ -21,7 +82,7 @@ const i18n = new VueI18n({
 });
 
 //使用钩子函数对路由进行权限跳转
-router.beforeEach((to, from, next) => {
+/*router.beforeEach((to, from, next) => {
     document.title = `${to.meta.title} | vue-manage-system`;
     const role = localStorage.getItem('ms_username');
     if (!role && to.path !== '/login') {
@@ -39,7 +100,23 @@ router.beforeEach((to, from, next) => {
             next();
         }
     }
+});*/
+
+
+/*
+router.afterEach(route => {
+    iView.LoadingBar.finish();
 });
+
+new Vue({
+    el: '#app',
+    router,
+    store, //注册组件
+    components: { App },
+    template: '<App/>'
+}) ;
+*/
+
 
 new Vue({
     router,
--- src/router/index.js
+++ src/router/index.js
@@ -19,68 +19,42 @@ export default new Router({
                     component: () => import(/* webpackChunkName: "dashboard" */ '../components/page/Dashboard.vue'),
                     meta: { title: '系统首页' }
                 },
-                {
-                    path: '/icon',
-                    component: () => import(/* webpackChunkName: "icon" */ '../components/page/Icon.vue'),
-                    meta: { title: '自定义图标' }
+                {   //app权限管理
+                    path: '/appPower',
+                    component: () => import(/* webpackChunkName: "icon" */ '../components/page/appPower.vue'),
+                    meta: { title: 'app权限管理' }
                 },
-                {
-                    path: '/table',
-                    component: () => import(/* webpackChunkName: "table" */ '../components/page/BaseTable.vue'),
-                    meta: { title: '基础表格' }
+                {   //部门管理
+                    path: '/dept',
+                    component: () => import(/* webpackChunkName: "OrganizationTree" */ '../components/page/dept.vue'),
+                    meta: { title: '部门管理' }
                 },
-                {
-                    path: '/tabs',
-                    component: () => import(/* webpackChunkName: "tabs" */ '../components/page/Tabs.vue'),
-                    meta: { title: 'tab选项卡' }
+                {   //员工信息
+                    path: '/deptUser',
+                    component: () => import(/* webpackChunkName: "User" */ '../components/page/deptUser.vue'),
+                    meta: { title: '员工信息' }
                 },
-                {
-                    path: '/form',
-                    component: () => import(/* webpackChunkName: "form" */ '../components/page/BaseForm.vue'),
-                    meta: { title: '基本表单' }
+
+                {   //用户管理
+                    path: '/SysUser',
+                    component: () => import(/* webpackChunkName: "tabs" */ '../components/page/SysUser.vue'),
+                    meta: { title: '用户管理' }
                 },
+
                 {
-                    // 富文本编辑器组件
-                    path: '/editor',
-                    component: () => import(/* webpackChunkName: "editor" */ '../components/page/VueEditor.vue'),
-                    meta: { title: '富文本编辑器' }
+                    // 考勤规则
+                    path: '/rule',
+                    component: () => import(/* webpackChunkName: "editor" */ '../components/page/rule.vue'),
+                    meta: { title: '考勤规则' }
                 },
+
                 {
-                    // markdown组件
-                    path: '/markdown',
-                    component: () => import(/* webpackChunkName: "markdown" */ '../components/page/Markdown.vue'),
-                    meta: { title: 'markdown编辑器' }
+                    // 门禁配置
+                    path: '/DoorControl',
+                    component: () => import(/* webpackChunkName: "upload" */ '../components/page/DoorControl.vue'),
+                    meta: { title: '门禁配置' }
                 },
-                {
-                    // 图片上传组件
-                    path: '/upload',
-                    component: () => import(/* webpackChunkName: "upload" */ '../components/page/Upload.vue'),
-                    meta: { title: '文件上传' }
-                },
-                {
-                    // vue-schart组件
-                    path: '/charts',
-                    component: () => import(/* webpackChunkName: "chart" */ '../components/page/BaseCharts.vue'),
-                    meta: { title: 'schart图表' }
-                },
-                {
-                    // 拖拽列表组件
-                    path: '/drag',
-                    component: () => import(/* webpackChunkName: "drag" */ '../components/page/DragList.vue'),
-                    meta: { title: '拖拽列表' }
-                },
-                {
-                    // 拖拽Dialog组件
-                    path: '/dialog',
-                    component: () => import(/* webpackChunkName: "dragdialog" */ '../components/page/DragDialog.vue'),
-                    meta: { title: '拖拽弹框' }
-                },
-                {
-                    // 国际化组件
-                    path: '/i18n',
-                    component: () => import(/* webpackChunkName: "i18n" */ '../components/page/I18n.vue'),
-                    meta: { title: '国际化' }
-                },
+
                 {
                     // 权限页面
                     path: '/permission',
@@ -88,26 +62,26 @@ export default new Router({
                     meta: { title: '权限测试', permission: true }
                 },
                 {
-                    path: '/404',
-                    component: () => import(/* webpackChunkName: "404" */ '../components/page/404.vue'),
-                    meta: { title: '404' }
+                    //vip管理
+                    path: '/vip',
+                    component: () => import(/* webpackChunkName: "vipTable" */ '../components/page/vip.vue'),
+                    meta: { title: 'vip' }
                 },
-                {
-                    path: '/403',
-                    component: () => import(/* webpackChunkName: "403" */ '../components/page/403.vue'),
-                    meta: { title: '403' }
+                {   //设备管理
+                    path: '/equipment',
+                    component: () => import(/* webpackChunkName: "403" */ '../components/page/equipment.vue'),
+                    meta: { title: '设备管理' }
                 },
-                {
-                    path: '/donate',
-                    component: () => import(/* webpackChunkName: "donate" */ '../components/page/Donate.vue'),
-                    meta: { title: '支持作者' }
-                }
+
             ]
         },
         {
             path: '/login',
             component: () => import(/* webpackChunkName: "login" */ '../components/page/Login.vue'),
-            meta: { title: '登录' }
+            meta: { title: '登录',
+                    requireAuth:true
+            }
+
         },
         {
             path: '*',
